2591c08199c1cecb0137e579cb5d93571401db1a
mitpatel3008 <mit.p30082@gmail.com>
2026-03-25 09:14:08 +0530

    resume updated

--- app/page.tsx
+++ app/page.tsx
@@ -184,7 +184,7 @@ export default function Portfolio() {
                   className="border-black text-black hover:bg-black hover:text-white bg-transparent"
                 >
                   <a
-                    href="/MitResume.pdf"
+                    href="/Resume.pdf"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
